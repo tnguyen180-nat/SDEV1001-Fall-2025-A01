beb1a749c2b97a0fb8566f177f06f5b79ce93d7b
Jared <jkarpiak@nait.ca>
2025-10-06 13:21:51 -0600

    add day 13

---
+++ Day 13/hunting_game_start.py
@@ -0,0 +1,5 @@
+import random
+HIT_LIMIT = 9
+
+available_monsters = ("zombie", "wraith", "bandit", "ghoul", "sphinx", "venemous gerbil", "chimera")
+weapons = ["crossbow", "nail clippers", "knife", "gun", "rubber chicken", "vitamins", "sword"]
